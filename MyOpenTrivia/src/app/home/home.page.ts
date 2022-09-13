971ca811c22a3e17d27c924c9bd77f3849668d3a
Dominique.amps@gmail.com <62357926+BiliennRuz@users.noreply.github.com>
2022-09-13 12:16:29 +0200

    fin TP module 5

--- MyOpenTrivia/src/app/home/home.page.ts
+++ MyOpenTrivia/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,28 +11,29 @@ export class HomePage {
   pseudo: string = "";
   level: String = "";
   errorMessage: string = "";
-  ErrorIsHidden: boolean = true;
+  ErrorIsHidden: boolean = false;
+  colorText: string = "blue";
   colorTextError: string = "red";
   formIsHidden: boolean = false;
-  questionIsHidden: boolean = true;
+  questionIsHidden: boolean = false;
   question: string = "De quelle couleur est le cheval blanc d'Henry IV";
   reponses: string[] = ["Blanc", "Noir", "Marron", "Je ne sais pas"];
-  questionSuivanteIsHidden: boolean = true;
+  questionSuivanteIsHidden: boolean = false;
 
   
-  constructor() {}
+  constructor(private alertCtrl: AlertController, private toastCtrl: ToastController) {}
 
   clickMe() {
     if (this.pseudo.length <3) {
       this.ErrorIsHidden = false;
-      this.errorMessage = "Veuillez rentrer un pseudo de 3 caractère minimum !";
+      this.showAlertPseudo();
     } else {
       this.ErrorIsHidden = true;
     }
 
     if (this.level=="") {
       this.ErrorIsHidden = false;
-      this.errorMessage = "Veuillez selectionner un niveau !";
+      this.showAlertLevel();
     } else {
       this.ErrorIsHidden = true;
     }
@@ -45,6 +47,33 @@ export class HomePage {
 
   clickRequest() {
     this.questionSuivanteIsHidden = false;
+  }
+
+  async showAlertPseudo() {
+    const alert = await this.alertCtrl.create({
+      header: "ALERTE !",
+      message: "Veuillez rentrer un pseudo de 3 caractère minimum !",
+      buttons: ['OK'],
+    });
+    alert.present();
+  }
+
+  async showAlertLevel() {
+    const alert = await this.alertCtrl.create({
+      header: "ALERTE !",
+      message: "Veuillez selectionner un niveau !",
+      buttons: ['OK'],
+    });
+    alert.present();
+  }
+
+  async showToast() {
+    const toast = await this.toastCtrl.create({
+      message: 'Votre réponse est : A',
+      duration: 5000,
+      position: 'bottom'
+    });
+    toast.present();
   }
 
 }
