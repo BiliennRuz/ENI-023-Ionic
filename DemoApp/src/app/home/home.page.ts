ca2677c870641fe7cd328dae5e6a5300e5ec65c5
Dominique.amps@gmail.com <62357926+BiliennRuz@users.noreply.github.com>
2022-09-14 14:01:49 +0200

    demo module 7

--- DemoApp/src/app/home/home.page.ts
+++ DemoApp/src/app/home/home.page.ts
@@ -9,11 +9,30 @@ import { DataService } from '../services/data.service';
 export class HomePage {
 
   nb: number = 0;
+  kittenImg: string = '';
 
   constructor(private dataSrv: DataService) {
     console.log("Home Page");
     this.dataSrv.str = 'Hello';
     console.log(this.dataSrv.str);
+  }
+
+  // Traitement du résultat d'une requête, retournée par le service, grâce au mot-clé then
+  getKitten() {
+    this.dataSrv.getCat().then((img: any) => {
+      if (img.length > 1) {
+        this.kittenImg = img;
+      }
+    });
+  }
+
+  // Traitement du résultat d'une requête, retournée par le service, grâce aux mots-clés async / await
+  async getKittenAsync() {
+    try {
+      this.kittenImg = await this.dataSrv.getCat();
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   // Récupération d'un nombre aléatoire du service généré de manière synchrone
--- DemoApp/src/app/services/data.service.ts
+++ DemoApp/src/app/services/data.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,9 +7,20 @@ import { Injectable } from '@angular/core';
 export class DataService {
 
   str: string = '';
+  baseUrl: string = "https://cataas.com";
 
-  constructor() {
+  constructor(private http: HttpClient) {
     console.log("Data Service");
+  }
+
+  getCat(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      this.http.get(this.baseUrl + "/cat?json=true").toPromise().then((result: any) => {
+        console.log(result);
+        resolve(this.baseUrl + result.url);
+        reject("Impossible de récupérer l'image : vérifiez votre connexion internet.");
+      });
+    });
   }
 
   getRandomNumber() {
