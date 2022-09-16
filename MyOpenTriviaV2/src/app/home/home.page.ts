7978a071f900a153c178fd494d2cac385be1ab26
Dominique.amps@gmail.com <62357926+BiliennRuz@users.noreply.github.com>
2022-09-16 16:00:29 +0200

    fin TP module 9

--- MyOpenTriviaV2/src/app/home/home.page.ts
+++ MyOpenTriviaV2/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController, ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { Preferences } from '@capacitor/preferences';
 
 
 @Component({
@@ -13,7 +14,13 @@ export class HomePage {
   pseudo: string = '';
   difficulties: string[] = ['easy', 'medium', 'hard'];
   difficulty: string = 'easy';
+  save: boolean = true;
 
+  async ngOnInit() {
+      const { value } = await Preferences.get({ key: 'pseudo' });
+      this.pseudo = value;
+      console.log('recup pseudo :' + this.pseudo);
+  }
 
   constructor(private alertCtrl: AlertController,
     private toastCtrl: ToastController,
@@ -21,6 +28,32 @@ export class HomePage {
 
   async begin() {
     if (this.pseudo.length >= 3) {
+
+      console.log(this.save);
+      if (this.save) {
+        //const setPseudo = async () => {
+          await Preferences.set({
+            key: 'pseudo',
+            value: this.pseudo,
+          });
+        //};
+        console.log('save pseudo: ' + this.pseudo);
+        //const setDifficulty = async () => {
+          await Preferences.set({
+            key: 'difficulty',
+            value: this.difficulty
+          });
+          console.log('save difficulty: ' + this.difficulty);
+        //};
+      } else {
+        const removePseudo = async () => {
+          await Preferences.remove({ key: 'pseudo' });
+        };
+        const removeDifficulty = async () => {
+          await Preferences.remove({ key: 'difficulty' });
+        };
+      }
+      
       //this.hideForm = true;
       //this.loadListQuestions();
       this.router.navigate(['/game', this.pseudo, this.difficulty]);
